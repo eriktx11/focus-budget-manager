4c0683cebb07fd588115fac79a4c90414d38ab70
eriktx11 <erik@llerenapiano.com>
2019-05-21 09:15:51 -0700

    Database Schema

--- services/BudgetManagerAPI/app/models/user.js
+++ services/BudgetManagerAPI/app/models/user.js
@@ -14,3 +14,22 @@ const Schema = mongoose.Schema({
     clients: [{}]
 }); 
 
+// We won't use arrow functions here because of automatic lexical scope binding
+Schema.pre('save', function (next){
+    const user = this;
+
+    if(this.isModified('password') || this.isNew){
+        bcrypt.genSalt(10, (error, salt) => {
+            if (error) return next(error);
+        
+        bcrypt.hash(user.password, salt, (error, hash) => {
+            if(error) return next(error);
+
+            user.password = hash;
+            next();
+            });
+        });
+    }else{
+        return next();
+    }
+});
