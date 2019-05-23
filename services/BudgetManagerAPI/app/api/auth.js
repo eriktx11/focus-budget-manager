a32b81e8e75d4808171fa647badac4af0254fc26
eriktx11 <erik@llerenapiano.com>
2019-05-22 22:46:24 -0500

    Authenticating user

--- services/BudgetManagerAPI/app/api/auth.js
+++ services/BudgetManagerAPI/app/api/auth.js
@@ -8,6 +8,16 @@ api.login = (User) => (req, res) => {
     User.findOne({ username:req.body.username }, (error, user) => {
         if(error) throw error;
 
-        if(!user) res.status(401).send({})
-    })
+        if(!user) res.status(401).send({ success: false, message: 'Authentication failed. User not found.'});
+        else {
+            user.comparePassword(req.body.password, (error, matches) => {
+                if (matches && !error){
+                    const token = jw.sign({ user }, config.secret);
+                    res.json({ success: true, message: 'Token granted', token});
+                }else{
+                    res.status(401).send({ success: false, message: 'Authentification failed. Wrong password.'});
+                }
+            });
+        }
+    });
 }
